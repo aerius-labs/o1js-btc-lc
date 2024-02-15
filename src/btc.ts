8f44ae0d5d98d430500d086a228c7577e308c4d3
Rahul Ghangas <rahulghangas0@gmail.com>
2024-02-15 16:58:31 +0530

    feat: modular exponentiation and create target

--- src/btc.ts
+++ src/btc.ts
@@ -1,5 +1,9 @@
 import { Field, UInt32, Provable, Hash, Struct } from 'o1js';
 import { Bytes, Gadgets, UInt8 } from 'o1js';
+import { createForeignField } from 'o1js';
+import { modExp } from './utils';
+
+class Field256 extends createForeignField(1n << 256n) {}
 
 // Define a provable type for 256-bit unsigned integers
 class UInt256 extends Struct({
@@ -48,13 +52,13 @@ class BtcHeader extends Struct({
   validate() {
 
     const hash = this.hash();
+    const hashBits = hash.toFields().flatMap((x) => x.toBits(8));
+    const hashU256 = Field256.fromBits(hashBits).assertCanonical();
 
     const target = this.bitsToTarget();
 
     // Check if the hash is less than the target
 
-    // hash. (target);
-    hash.bytes
 
   }
 
@@ -71,11 +75,17 @@ class BtcHeader extends Struct({
 
     // Calculate the target
     // const target = modExp(coefficient.value, exponent.sub(Field.from(3)).mul(Field.from(8)));
+    const expU32 = modExp(coefficient.value, exponent.sub(Field.from(3)));
+    const exp = Field256.fromBits(expU32.toBits());
 
-    return coefficient;
+    const multiplicand = new Field256.AlmostReduced(8);
+    const target = exp.mul(multiplicand);
+    return target.assertCanonical();
   }
 
 }
+
+//
 
 
 export { BtcHeader };
