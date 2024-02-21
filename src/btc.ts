c905f6abdda8356d88f6f4166bd47ea8f639e771
Rahul Ghangas <rahulghangas0@gmail.com>
2024-02-21 12:12:12 +0530

    fix: type and target calculation logic

--- src/btc.ts
+++ src/btc.ts
@@ -1,4 +1,4 @@
-import { Field, UInt32, Provable, Hash, Struct, Bool } from 'o1js';
+import { Field, UInt32, Provable, Hash, Struct, Bool, CanonicalForeignField } from 'o1js';
 import { Bytes, Gadgets, UInt8 } from 'o1js';
 import { createForeignField } from 'o1js';
 import { modExp } from './utils';
@@ -10,26 +10,38 @@ class Field257 extends createForeignField(1n << 257n) {}
 class Bytes80 extends Bytes(80) {}
 class Bytes32 extends Bytes(32) {}
 
+class Bytes4 extends Bytes(4) {}
+
 // Define a struct for the Bitcoin block header
 // @ts-ignore
 class BtcHeader extends Struct({
-  version: UInt32,
-  prevBlock: Bytes32,
-  merkleRoot: Bytes32,
+  version: Bytes4.provable,
+  prevBlock: Bytes32.provable,
+  merkleRoot: Bytes32.provable,
   timestamp: UInt32,
   bits: UInt32,
-  nonce: UInt32
+  nonce: Bytes4.provable
 
 }) {
   // Method to serialize the header into bytes
   toBytes() {
     return new Bytes80([
-      ...this.version.toFields().map((x) => UInt8.from(x)),
-      ...this.prevBlock.provable.bytes,
-      ...this.merkleRoot.provable.bytes,
-      ...this.timestamp.toFields().map((x) => UInt8.from(x)),
-      ...this.bits.toFields().map((x) => UInt8.from(x)),
-      ...this.nonce.toFields().map((x) => UInt8.from(x))
+      ...this.version.bytes,
+      ...this.prevBlock.bytes,
+      ...this.merkleRoot.bytes,
+      ...Bytes(4).from([
+        UInt8.from(this.timestamp.and(UInt32.from(0xff))),
+        UInt8.from(this.timestamp.and(UInt32.from(0xff00)).rightShift(8)),
+        UInt8.from(this.timestamp.and(UInt32.from(0xff0000)).rightShift(16)),
+        UInt8.from(this.timestamp.and(UInt32.from(0xff000000)).rightShift(24))
+      ]).bytes,
+      ...Bytes(4).from([
+        UInt8.from(this.bits.and(UInt32.from(0xff))),
+        UInt8.from(this.bits.and(UInt32.from(0xff00)).rightShift(8)),
+        UInt8.from(this.bits.and(UInt32.from(0xff0000)).rightShift(16)),
+        UInt8.from(this.bits.and(UInt32.from(0xff000000)).rightShift(24))
+      ]).bytes,
+      ...this.nonce.bytes
     ]);
 
   }
@@ -45,7 +57,6 @@ class BtcHeader extends Struct({
   // Method to validate the header
 
   public validate() {
-
     const hash = this.hash();
     const hashBits = hash.toFields().flatMap((x) => x.toBits(8));
     const hashU257 = Field257.fromBits(hashBits).assertCanonical();
@@ -59,19 +70,14 @@ class BtcHeader extends Struct({
   // Method to convert bits field to target
   bitsToTarget() {
 
-    // Extract the exponent and coefficient from the bits field
-
+    // Extract the exponent and significand from the bits field
     const exponent = Gadgets.rightShift64(this.bits.value, 24);
-
-    const coefficient = this.bits.and(UInt32.from(0x00ffffff));
+    const significand = this.bits.and(UInt32.from(0x00ffffff));
 
     // Calculate the target
-    // const target = modExp(coefficient.value, exponent.sub(Field.from(3)).mul(Field.from(8)));
-    const expU32 = modExp(coefficient.value, exponent.sub(Field.from(3)));
-    const exp = Field257.fromBits(expU32.toBits());
-
-    const multiplicand = new Field257.AlmostReduced(8);
-    const target = exp.mul(multiplicand);
+    const expField = modExp(Field.from(256), exponent.sub(Field.from(3)));
+    const exp = Field257.fromBits(expField.toBits());
+    const target = exp.mul(Field257.fromBits(significand.value.toBits()));
     return target.assertCanonical();
   }
 
@@ -80,9 +86,9 @@ class BtcHeader extends Struct({
 const checkTarget = (hash: Field257, target: Field257) => {
   const full = new Field257(1n << 256n);
   const diff = full.sub(target);
-  const val = diff.add(hash);
+  const val = diff.add(hash).assertCanonical();
   const bits = val.toBits();
   bits[256].assertFalse("hash is greater than target");
 }
 
-export { BtcHeader, Field257, Bytes80, Bytes32 };
+export { BtcHeader, Field257, Bytes80, Bytes32, Bytes4 };
