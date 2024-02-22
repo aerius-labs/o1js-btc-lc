b8d04aa916d7d039142176b9bb7013c07c73c8cd
Rahul Ghangas <rahulghangas0@gmail.com>
2024-02-22 16:42:03 +0530

    test: fix BtcHeader initialization

--- src/btc.test.ts
+++ src/btc.test.ts
@@ -1,4 +1,4 @@
-import { BtcHeader, Bytes32, Field257 } from './btc';
+import { BtcHeader, Bytes32, Bytes4 } from './btc';
 import { Bytes, UInt32 } from 'o1js';
 
 // Example header
@@ -14,26 +14,27 @@ import { Bytes, UInt32 } from 'o1js';
 // fe9f0864 ........................... Nonce
 describe('Bitcoin Header', () => {
   it('Acceptable header should be validated correctly', () => {
-    const version = UInt32.from(0x02000000);
+    const versionBytes = Bytes.fromHex('02000000');
+    const version = Bytes4.from(versionBytes);
     const prevBlockBytes = Bytes.fromHex('b6ff0b1b1680a2862a30ca44d346d9e8910d334beb48ca0c0000000000000000');
-    const prevBlock = new Bytes32(prevBlockBytes.bytes);
+    const prevBlock = Bytes32.from(prevBlockBytes);
     const merkleRootBytes = Bytes.fromHex('9d10aa52ee949386ca9385695f04ede270dda20810decd12bc9b048aaab31471');
-    const merkleRoot = new Bytes32(merkleRootBytes.bytes);
+    const merkleRoot = Bytes32.from(merkleRootBytes);
     const timestamp = UInt32.from(0x24d95a54);
     const bits = UInt32.from(0x30c31b18);
-    const nonce = UInt32.from(0xfe9f0864);
+
+    let nonceBytes = Bytes.fromHex('fe9f0864');
+    let nonce = Bytes4.from(nonceBytes);
 
     const header = new BtcHeader({
-      version,
-      // @ts-ignore
-      prevBlock,
-      // @ts-ignore
-      merkleRoot,
-      timestamp,
-      bits,
-      nonce
+      version: version,
+      prevBlock: prevBlock,
+      merkleRoot: merkleRoot,
+      timestamp: timestamp,
+      bits: bits,
+      nonce: nonce
     });
 
-    // header.validate();
+    header.validate();
   });
 });
